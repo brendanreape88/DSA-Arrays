ace74f6940545ff3ae0be82e7ffb01058ee19fac
Brendan Reape <brendanreape88@gmail.com>
2020-09-22 10:08:08 -0700

    basic functions working

--- drills.js
+++ drills.js
@@ -1,21 +1,43 @@
 const Memory = require("Memory");
 
-class Array {
+const MEMORY = new Memory();
+
+class MyArray {
   constructor() {
+    const INITIAL_CAPACITY = 4;
+
+    this._capacity = INITIAL_CAPACITY;
+    this._startAddress = MEMORY.allocate(INITIAL_CAPACITY);
     this.length = 0;
-    this.ptr = memory.allocate(this.length);
+  }
+
+  _resize() {
+    const newStart = MEMORY.allocate(this._capacity * 2);
+    MEMORY.copy(newStart, this._startAddress, this.length);
+    MEMORY.free(this._startAddress);
+    this._startAddress = newStart;
+    this._capacity *= 2;
+    console.log(this._startAddress, MEMORY.get(newStart + 3));
+  }
+
+  push(el) {
+    if (this.length === this._capacity) this._resize();
+
+    const memoryPtr = this._startAddress + this.length;
+    MEMORY.set(memoryPtr, el);
+    this.length++;
+  }
+
+  get(idx) {
+    const memoryAddr = this._startAddress + idx;
+    return MEMORY.get(memoryAddr);
   }
 }
 
-function main() {
-  Array.SIZE_RATIO = 3;
-  let arr = new Array();
-  arr.push(3);
-  arr.pop();
-  arr.pop();
-  arr.pop();
-  console.log(arr);
+let arr = new MyArray();
+
+for (let i = 0; i < 65; i++) {
+  arr.push(i);
 }
 
-//unify a string
-function unify() {}
+console.log(arr.length, arr._startAddress, arr._capacity, arr.get(50));
